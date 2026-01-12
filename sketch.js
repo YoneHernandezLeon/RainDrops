b645a1e2e60752840093c3b33f3a497554fae6ea
Yone Hernández León <43843420+YoneHernandezLeon@users.noreply.github.com>
2026-01-12 13:46:21 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 let d=[]
 let co=0
 function setup(){
-  createCanvas(400,400,WEBGL)
-  for(let i=0;i<400;i++){
+  createCanvas(800,800,WEBGL)
+  for(let i=0;i<600;i++){
     d[i]=new drop()
   }
   b=new barrier()
@@ -11,19 +11,27 @@ function draw() {
   background(0,9,57)
   translate(-width/2,-height/2,-50)
   rotateX(radians(20))
-  for(let i=0;i<400;i++){
-    strokeWeight(d[i].w)
+  for(let i=0;i<600;i++){
+    strokeWeight(1.5)
     if(d[i].x>=b.x&&d[i].x<=b.x+b.s&&d[i].y>b.y){
-      stroke(30)
-    }else{
+      d[i].c=b.c
+      d[i].flag = true
+    }
+    if(d[i].flag){
+      stroke(200)
+    } else {
       stroke(d[i].c)
     }
-    line(d[i].x,d[i].y,d[i].x,d[i].y+d[i].s);
+    fill(d[i].c)
+    rect(d[i].x,d[i].y,d[i].w,d[i].s);
     d[i].fall();
-    if (d[i].y>415){d[i]=new drop()}
+    if (d[i].y>815){d[i]=new drop()}
   }
-  stroke(255)
-  strokeWeight(6)
+  fill(b.c[0], b.c[1], b.c[2], 20)
+  strokeWeight(0)
+  rect(b.x,b.y,b.s,780);
+  stroke(b.c)
+  strokeWeight(7)
   line(b.x,b.y,b.x+b.s,b.y);
   co++;
   if(co==200){
@@ -34,19 +42,21 @@ function draw() {
 class drop{
   constructor(){
     this.s=random(6,12)
-    this.x=random(-50,450)
+    this.x=random(-50,850)
     this.y=random(-50, -40)
     this.c=[random(255), random(255), random(255)]
     this.w=random([2,3,4])
+    this.flag=false
   }
   fall(){
-    this.y+=this.s/2+this.y/100
+    this.y+=this.s+this.y/200
   }
 }
 class barrier{
   constructor(){
-    this.x = random(20, 300)
+    this.x = random(20, 750)
     this.y = random(20, 50)
     this.s = random(100, 200)
+    this.c = [random(255), random(255), random(255)]
   }
 }
